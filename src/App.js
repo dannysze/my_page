85da4bf5abfd0598eaa31daa49810ba89690d478
dannysze <dann.nn49825@gmail.com>
2021-12-28 23:54:26 +0800

    footer update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </> :
         <Maintainanace />
       }
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,37 @@
-import React  from "react";
+import React from "react";
+import { Google, Github, Linkedin, Instagram } from 'react-bootstrap-icons';
 import './Footer.sass';
 
 const Footer = () => {
+
   return (
-    <div className="footer">
-      This is my footer
-    </div>
+    <footer className="footer flex__container__center">
+      <div className="footer__container">
+        <div className="footer-icons__ribbon flex__container__center">
+          {/* <div className="footer-icons__containers"> */}
+            <a href="#" className="footer-icons__link flex__container__center">
+              <Google />
+            </a>
+          {/* </div> */}
+          {/* <div className="footer-icons__containers"> */}
+            <a href="https://github.com/dannysze" className="footer-icons__link flex__container__center">
+              <Github />
+            </a>
+          {/* </div> */}
+          {/* <div className="footer-icons__containers"> */}
+            <a href="https://www.linkedin.com/in/danny-sze-049a5b168/" className="footer-icons__link flex__container__center">
+              <Linkedin />
+            </a>
+          {/* </div> */}
+          {/* <div className="footer-icons__containers"> */}
+            <a href="https://www.instagram.com/dannnccccc/" className="footer-icons__link flex__container__center">
+              <Instagram />
+            </a>
+          {/* </div> */}
+        </div>
+        <p className="footer__text small-1 center">Danny Sze &copy; 2022</p>
+      </div>
+    </footer>
   )
 }
 
