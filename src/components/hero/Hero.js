653f58ea86f85ea638ac62cfd6bd0b34f5dd9d1b
dannysze <danny.nn49825@gmail.com>
2022-01-05 22:28:34 +0800

    minor updates

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './Hero.sass';
 
 const Hero = () => {
+
+  const first = <h1>Hi everyone! I'm</h1>;
+  const second = <h2>Danny SZE.</h2>;
+  const third = <h3>I Believe I can build anything.</h3>;
+  const heroPassage = <p>I will graduate from The Chinese University of Hong Kong in July, 2022. Currently, I'm a part-time full stack developer at iPYGG.</p>;
+
+  const heroItems = [first, second, third, heroPassage];
+
   return (
-    <section id="hero">
+    <section id="hero" className="flex__container__center">
       <div className="max-width__container">
-        
+        {heroItems.map((item, idx) => (
+          <div key={idx}>
+            {item}
+          </div>
+        ))}
       </div>
     </section>
   );  
